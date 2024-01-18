31f7bd13f8cd9bdd773eee6eeac2bf1ccde3753b
saudurhasibuan <saudurh@gmail.com>
2024-01-19 05:55:44 +0700

    first commit

--- tests/Feature/DebitCardControllerTest.php
+++ tests/Feature/DebitCardControllerTest.php
@@ -3,6 +3,9 @@
 namespace Tests\Feature;
 
 use App\Models\User;
+use App\Models\DebitCard;
+use Carbon\Carbon;
+use Illuminate\Http\Client\Response;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Laravel\Passport\Passport;
 use Tests\TestCase;
@@ -31,16 +34,28 @@ class DebitCardControllerTest extends TestCase
         // $debitCard = factory(DebitCard::class)->create(['user_id' => $user->id]);
         // $transaction = factory(DebitCardTransaction::class)->create(['debit_card_id' => $debitCard->id]);
 
-        $response = $this->actingAs($user)
-            ->json('GET', '/debit-cards');
-
-        $response->assertStatus(200)
-            ->assertJsonFragment(['id' => $transaction->id]);
+        $response = $this->get('/debit-cards')
+        ->assertJsonStructure([
+            'status',
+            'result' => [
+                'number',
+                'type',
+                'expiration_date',
+            ],
+        ]);
+    $response->assertStatus(200);
     }
 
     public function testCustomerCannotSeeAListOfDebitCardsOfOtherCustomers()
     {
         // get /debit-cards
+
+        $response = $this->get('/debit-cards')
+        ->assertFalse();
+
+        $debitCards = (array)json_decode($this->response->content());
+        $response->assertContains($this->user->id, $debitCards['user_id']);
+
     }
 
     public function testCustomerCanCreateADebitCard()
@@ -52,16 +67,43 @@ class DebitCardControllerTest extends TestCase
     public function testCustomerCanSeeASingleDebitCardDetails()
     {
         // get api/debit-cards/{debitCard}
+        $this->post('/debit-cards', [
+            'user_id' => $this->user->id,
+            'type' => 'type 1',
+            'number' => rand(1000000000000000, 9999999999999999),
+            'expiration_date' => Carbon::now()->addYear(),
+        ]);
+
+        $newDebitCard = (array)json_decode($this->response->content());
+
+        $this->assertArrayHasKey('user_id', $newDebitCard);
+        $this->assertCount(
+            1,
+            $this->user->debitCards->latest()->count(), "1 data has been added"
+        );
+        $this->assertEquals($newDebitCard['number'], $this->user->debitCards->get());
     }
 
     public function testCustomerCannotSeeASingleDebitCardDetails()
     {
         // get api/debit-cards/{debitCard}
+        $debitCardId = 1;
+        $response = $this->get('/debit-cards', [
+            'id' => $debitCardId,
+        ]);
+
+        $debitCards = (array)json_decode($this->response->content());
+        $response->assertNull( 
+            $debitCards, 
+            "Data not available"
+        ); 
     }
 
     public function testCustomerCanActivateADebitCard()
     {
         // put api/debit-cards/{debitCard}
+        
+        
     }
 
     public function testCustomerCanDeactivateADebitCard()
